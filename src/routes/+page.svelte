829015d57e022f27c53b841c09b8cce7c488b11f
grizhlieCodes <rafal.potasz@gmail.com>
2023-05-21 23:11:38 +0100

    new test form

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    	export const prerender = true
+</script>
+
+
+<form name="contact-two" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact-two" class="sr-only"/>
+    <input name="name" type="text" required />
+    <input name="email" type="email" required />
+    <button type="submit">Submit</button>
+</form>
